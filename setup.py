da06563ca4650e6c7dc16575f46f08b649bed54c
Geoff Hing <geoffhing@gmail.com>
2016-04-15 17:48:15 -0500

    Make version detection work in Python 3
    My previous fix for getting the version for the call to
    `setup()` without using import worked in Python 2.7, but not
    Python 3.*.  Use a pattern that will work in both.

--- setup.py
+++ setup.py
@@ -1,17 +1,19 @@
 from distutils.core import setup
-import pkg_resources
 
+import os.path
 import sys
 
 requires = ['six']
 if sys.version_info < (2, 7):
     requires.append('argparse')
 
-version = pkg_resources.require("fitparse")[0].version
+# Get __version__ from fitparse.version without importing
+with open(os.path.join('fitparse', 'version.py')) as f:
+    exec(f.read())
 
 setup(
     name='fitparse',
-    version=version,
+    version=__version__,
     description='Python library to parse ANT/Garmin .FIT files',
     author='David Cooper, Kévin Gomez',
     author_email='dave@kupesoft.com, contact@kevingomez.fr',
